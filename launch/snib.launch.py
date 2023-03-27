83889bb71cc6e3d7649ac7d1fb089b97b3030949
spyalexs <spyalexs@gmail.com>
2023-03-27 19:26:51 -0400

    Group Action Launch

--- launch/snib.launch.py
+++ launch/snib.launch.py
@@ -3,7 +3,7 @@ import launch.actions
 from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 import os
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, GroupAction
 from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
@@ -28,17 +28,18 @@ def generate_launch_description():
             description="wether or not to launch simulink"
             ),
 
-        # create the nodes    
-        launch_ros.actions.Node(
-            package='riptide_SNIB',
-            executable='SNIB',
-            name='riptide_SNIB',
-            respawn=True,
-            output='screen',
-            
-            # use the parameters on the node
-            parameters = [
-                config
-            ]
-        )
+        GroupAction([
+            # create the nodes    
+            launch_ros.actions.Node(
+                package='riptide_SNIB',
+                executable='SNIB',
+                name='riptide_SNIB',
+                respawn=True,
+                output='screen',
+                
+                # use the parameters on the node
+                parameters = [
+                    config
+                ]
+            )], scoped=True)
     ])
